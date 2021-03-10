bb4b32763c9ce1b0f306dbb254d1ac067d6aacc7
Nitesh <Nitesh@conceptinfoway.net>
2021-03-10 15:44:18 +0530

    Adding Main page to route between Axios and React-Query

--- organic-crypto-tracker/src/App.js
+++ organic-crypto-tracker/src/App.js
@@ -1,10 +1,20 @@
+import React from "react"
+// Purpose: Include routing related package
+// Created By: CIPL
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import './App.css';
+
+// Purpose: Include pages here
+// Created By: CIPL
+import Main from "./pages/main";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+    </Router>
   );
 }
 
