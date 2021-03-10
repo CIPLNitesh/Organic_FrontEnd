2f1e17acd2bab98d625bcbcf2a7231bd00ba5f73
Nitesh <Nitesh@conceptinfoway.net>
2021-03-10 16:12:13 +0530

    Added new page to show listing using Axios API with Search Filter

--- organic-crypto-tracker/src/App.js
+++ organic-crypto-tracker/src/App.js
@@ -7,12 +7,14 @@ import './App.css';
 // Purpose: Include pages here
 // Created By: CIPL
 import Main from "./pages/main";
+import CallAxios from "./pages/axios";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Main} />
+        <Route path="/axios" component={CallAxios} />
       </Switch>
     </Router>
   );
